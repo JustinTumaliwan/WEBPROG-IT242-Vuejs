56397bc09f668be88368dd9c5c6424b160365576
Justin Tumaliwan <jgtumaliwan@student.apc.edu.ph>
2026-01-16 11:04:13 +0800

    Removed html

--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -1,6 +1,4 @@
 <template>
-<!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -190,7 +188,6 @@
     </section>
 
 </body>
-</html>
 </template>
 
     <script src=".components/js/script.js"></script>
