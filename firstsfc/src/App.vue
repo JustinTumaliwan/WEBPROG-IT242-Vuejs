5017edf671fc9f0cfc38974f44c79d5211086453
Justin Tumaliwan <jgtumaliwan@student.apc.edu.ph>
2026-01-20 08:28:57 +0800

    Changed the components

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,78 +1,10 @@
-<script setup>
-import ProfileLayout from './components/ProfileLayout.vue';
-import InfoSection from './components/InfoSection.vue';
-</script>
-
 <template>
-  <div class="main-wrapper">
-    <h1>Personal Profile Web page</h1>
-    
-    <InfoSection>
-      <strong>About Me</strong><br>
-      Education / Achievements<br>
-      STEM-IT SHS - Asia Pacific College<br>
-      BSIT (SOCIT) - Asia Pacific College<br>
-      <strong>Course:</strong> BSIT<br>
-      <strong>IT Experience:</strong> Programming, Scrum Certificate, Coding<br>
-      <strong>Hobbies:</strong> Games, Music, Gardening, Keyboard Making
-    </InfoSection>
-
-    <ProfileLayout>
-      <template #header>Hobbies and Interests</template>
-      
-      <template #nav>
-        <ul class="nav-links">
-          <li><a href="https://en.wikipedia.org/wiki/Knives_Out">Knives Out</a></li>
-          <li><a href="https://en.wikipedia.org/wiki/Spider-Man:_No_Way_Home">No Way Home</a></li>
-          <li><a href="https://en.wikipedia.org/wiki/Spider-Man:_Across_the_Spider-Verse">Spider Verse</a></li>
-        </ul>
-      </template>
-
-      <template #content>
-        <h2>Movies and Impact</h2>
-        <p>Movies are impactful because they serve as a powerful mirror and molder of human experience. They possess a unique ability to transport viewers into new realities.</p>
-      </template>
-
-      <template #sidebar>
-        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/1/1f/Knives_Out_poster.jpeg/250px-Knives_Out_poster.jpeg" alt="knives out" class="responsive-img">
-      </template>
-
-      <template #ads>
-        <img src="https://upload.wikimedia.org/wikipedia/en/b/b4/Spider-Man-_Across_the_Spider-Verse_poster.jpg" alt="spider verse" class="responsive-img">
-      </template>
-
-      <template #footer>
-        The Movies above are one of my favorite movies to watch and binge
-      </template>
-    </ProfileLayout>
-  </div>
+  <profile-layout/>
+  <food-item/>
+  <food-item2/>
 </template>
 
+<script></script>
+
 <style>
-body {
-  background-color: lightgreen;
-  margin: 0;
-  padding: 20px;
-  font-family: Verdana, Geneva, sans-serif;
-}
-
-h1 {
-  color: navy;
-  text-align: center;
-  margin-bottom: 30px;
-}
-
-.responsive-img {
-  width: 100%;
-  height: auto;
-  max-width: 200px;
-  display: block;
-  margin: 0 auto;
-}
-
-.nav-links {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-}
 </style>
--- firstsfc/src/components/ProfileLayout.vue
+++ firstsfc/src/components/ProfileLayout.vue
@@ -1,65 +1,55 @@
 <template>
   <div class="container">
-    <header class="head"><slot name="header"></slot></header>
-    <nav class="nav"><slot name="nav"></slot></nav>
-    <main class="content"><slot name="content"></slot></main>
-    <aside class="sidebar"><slot name="sidebar"></slot></aside>
-    <aside class="ads"><slot name="ads"></slot></aside>
-    <footer class="footer"><slot name="footer"></slot></footer>
+    <h1>Personal Profile Web page</h1>
+    <p class="paragraph">
+      <strong>About Me</strong><br>
+      ( Do not include birthdate / age / contact information)<br>
+      Education / Achievements<br>
+      STEM-IT SHS - Asia Pacific College<br>
+      BSIT (SOCIT) - Asia Pacific College<br>
+      <strong>Course</strong><br>
+      BSIT<br>
+      <strong>IT Experience</strong><br>
+      Programming<br>
+      Scrum Certificate Passer<br>
+      Coding<br>
+      <strong>Hobbies & Interests</strong><br>
+      Playing games<br>
+      Listening to music<br>
+      Gardening<br>
+      Keyboard Making<br>
+      <strong>Goals In Life / Dream</strong><br>
+      To be successful and do all the things I couldnt do back then
+    </p>
+
+    <div class="grid-layout">
+      <header class="head">Hobbies and Interests</header>
+      <nav class="nav">
+        <ul>
+          <li><a href="https://en.wikipedia.org/wiki/Knives_Out">Knives Out</a></li>
+          <li><a href="https://en.wikipedia.org/wiki/Spider-Man:_No_Way_Home">No Way Home</a></li>
+          <li><a href="https://en.wikipedia.org/wiki/Spider-Man:_Across_the_Spider-Verse">Spider Verse</a></li>
+        </ul>
+      </nav>
+      <div class="content">
+        <h1>Movies and Impact</h1>
+        <p>
+          Movies are impactful because they serve as a powerful mirror and molder of human experience. <br>
+          They possess a unique ability to transport viewers into new realities, fostering deep empathy <br>
+          by allowing us to temporarily walk in the shoes of diverse characters facing varied struggles and triumphs.
+        </p>
+      </div>
+      <div class="sidebar">
+        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/1/1f/Knives_Out_poster.jpeg/250px-Knives_Out_poster.jpeg" alt="knives out" width="200" height="300">
+      </div>
+      <div class="ads">
+        <img src="https://upload.wikimedia.org/wikipedia/en/b/b4/Spider-Man-_Across_the_Spider-Verse_poster.jpg" alt="spider verse" width="200" height="300">
+      </div>
+      <footer class="footer">The Movies above are one of my favorite movies to watch and binge</footer>
+    </div>
   </div>
 </template>
 
-<style>
-.container {
-  display: grid;
-  grid-template-columns: repeat(12, [col-start] 1fr);
-  gap: 20px;
-  color: black;
-}
+<script></script>
 
-.container > * {
-  border: 1px solid black;
-  background-color: lightblue;
-  padding: 10px;
-  grid-column: col-start / span 12;
-}
-
-nav ul {
-  list-style: none;
-  margin: 0;
-  padding: 0;
-}
-
-@media (min-width: 576px) {
-  .sidebar {
-    grid-column: col-start / span 3;
-    grid-row: 3;
-  }
-  .ads {
-    grid-column: col-start / span 3;
-  }
-  .content, .footer {
-    grid-column: col-start 4 / span 9;
-  }
-}
-
-@media (min-width: 768px) {
-  .nav {
-    grid-column: col-start / span 2;
-    grid-row: 2 / 4;
-  }
-  .content {
-    grid-column: col-start 3 / span 8;
-    grid-row: 2 / 4;
-  }
-  .sidebar {
-    grid-column: col-start 11 / span 2;
-  }
-  .ads {
-    grid-column: col-start 11 / span 2;
-  }
-  .footer {
-    grid-column: col-start / span 12;
-  }
-}
-</style>
+<style src="./css/personalprofile.css"></style>
