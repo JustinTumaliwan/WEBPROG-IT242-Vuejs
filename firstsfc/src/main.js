f9914ba68f4c95c250b3bd1a2b82a64da68661cc
Justin Tumaliwan <jgtumaliwan@student.apc.edu.ph>
2026-01-27 10:51:29 +0800

    Added and inserted comment or feedback comment function

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -4,9 +4,13 @@ import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
 import ProfileLayout from './components/ProfileLayout.vue'
+import CommentForm from './components/CommentForm.vue'
+import Comment from './components/Comment.vue'
 
 const app = createApp(App)
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
 app.component('profile-layout', ProfileLayout)
+app.component('comment-form', CommentForm)
+app.component('comment', Comment)
 app.mount('#app')
